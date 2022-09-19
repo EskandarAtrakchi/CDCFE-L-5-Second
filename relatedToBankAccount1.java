1f8f007485f08630a70a278f5ff66bb7cbf1834f
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-19 21:58:02 +0100

    relatedToBankAccount1

--- relatedToBankAccount1.java
+++ relatedToBankAccount1.java
@@ -1,6 +1,6 @@
 /*
 private static String customersNames1[]=
-		    	{"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n"
+		    	{"\t\t\t\tThis is an actual bank statement of Bank\t\t\t\t\n"
 		    			+ "--------------------------------------------------------------------"
 		    			+ "-------------------------------------------"
 		    			+ "------\n\t\t\t\t\tNames\t\tPins\t\tBalances\n\n--------------------"
@@ -9,7 +9,7 @@ private static String customersNames1[]=
 		    			+ "rson1\t1\t\t111$\n\n\t\tThis balance has been taken before any tra"
 		    			+ "nsaction occures\n\n\t\t\tContact us for any queries at 01-999-392"
 		    			+ "\n\n\n\t\t---------------------------Have a nice day---------------------------\n",
-		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n---------------"
+		    	"\t\t\t\tThis is an actual bank statement of Bank\t\t\t\t\n---------------"
 		    	+ "--------------------------------------------"
 		    	+ "----------------------------------------------------------\n\t\t\t\t\tNames\t\tP"
 		    	+ "ins\t\tBalances\n\n------------------------------------------------------------"
@@ -17,7 +17,7 @@ private static String customersNames1[]=
 		    	+ "--------\n\n\t\t\t\t\tPerson2\t2\t\t222$\n\n\t\tThis balance has been taken befor"
 		    	+ "e any transaction occures\n\n\t\t\tContact us for any queries at 01-999-392\n\n\n\t\t"
 		    	+ "---------------------------Have a nice day---------------------------\n",
-		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n------"
+		    	"\t\t\t\tThis is an actual bank statement of Bank\t\t\t\t\n------"
 		    	+ "-------------------------------------------------------------"
 		    	+ "--------------------------------------------------\n\t\t\t\t\tNames\t\tPins\t\tBalances"
 		    	+ "\n\n------------------------------------------------------------------------------"
@@ -25,7 +25,7 @@ private static String customersNames1[]=
 		    	+ "Person3\t3\t\t333$\n\n\t\tThis balance has been taken before any transaction occures"
 		    	+ "\n\n\t\t\tContact us for any queries at 01-999-392\n\n\n\t\t--------------------------"
 		    	+ "-Have a nice day---------------------------\n",
-		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n---------------"
+		    	"\t\t\t\tThis is an actual bank statement of Bank\t\t\t\t\n---------------"
 		    	+ "----------------------------------------------------"
 		    	+ "--------------------------------------------------\n\t\t\t\t\tNames\t\tPins\t\tBalances"
 		    	+ "\n\n-------------------------------------------------------------------------------"
@@ -33,7 +33,7 @@ private static String customersNames1[]=
 		    	+ "Person4\t4\t\t444$\n\n\t\tThis balance has been taken before any transaction occures"
 		    	+ "\n\n\t\t\tContact us for any queries at 01-999-392\n\n\n\t\t------------------------"
 		    	+ "---Have a nice day---------------------------\n",
-		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n-----------------"
+		    	"\t\t\t\tThis is an actual bank statement of Bank\t\t\t\t\n-----------------"
 		    	+ "-------------------------------------------------"
 		    	+ "---------------------------------------------------\n\t\t\t\t\tNames\t\tPins\t\tBalances"
 		    	+ "\n\n-------------------------------------------------------------------------------"
