d928d36bcf4b23025d7c8cb1002cb298809e9521
Eskandar Atrakchi <102361045+EskandarAtrakchi@users.noreply.github.com>
2022-09-19 21:56:11 +0100

    relatedToBankAccount1

---
+++ relatedToBankAccount1.java
@@ -0,0 +1,45 @@
+/*
+private static String customersNames1[]=
+		    	{"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n"
+		    			+ "--------------------------------------------------------------------"
+		    			+ "-------------------------------------------"
+		    			+ "------\n\t\t\t\t\tNames\t\tPins\t\tBalances\n\n--------------------"
+		    			+ "------------------------------------------------"
+		    			+ "-------------------------------------------------\n\n\t\t\t\t\tPe"
+		    			+ "rson1\t1\t\t111$\n\n\t\tThis balance has been taken before any tra"
+		    			+ "nsaction occures\n\n\t\t\tContact us for any queries at 01-999-392"
+		    			+ "\n\n\n\t\t---------------------------Have a nice day---------------------------\n",
+		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n---------------"
+		    	+ "--------------------------------------------"
+		    	+ "----------------------------------------------------------\n\t\t\t\t\tNames\t\tP"
+		    	+ "ins\t\tBalances\n\n------------------------------------------------------------"
+		    	+ "-------------------------------------------------"
+		    	+ "--------\n\n\t\t\t\t\tPerson2\t2\t\t222$\n\n\t\tThis balance has been taken befor"
+		    	+ "e any transaction occures\n\n\t\t\tContact us for any queries at 01-999-392\n\n\n\t\t"
+		    	+ "---------------------------Have a nice day---------------------------\n",
+		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n------"
+		    	+ "-------------------------------------------------------------"
+		    	+ "--------------------------------------------------\n\t\t\t\t\tNames\t\tPins\t\tBalances"
+		    	+ "\n\n------------------------------------------------------------------------------"
+		    	+ "---------------------------------------\n\n\t\t\t\t\t"
+		    	+ "Person3\t3\t\t333$\n\n\t\tThis balance has been taken before any transaction occures"
+		    	+ "\n\n\t\t\tContact us for any queries at 01-999-392\n\n\n\t\t--------------------------"
+		    	+ "-Have a nice day---------------------------\n",
+		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n---------------"
+		    	+ "----------------------------------------------------"
+		    	+ "--------------------------------------------------\n\t\t\t\t\tNames\t\tPins\t\tBalances"
+		    	+ "\n\n-------------------------------------------------------------------------------"
+		    	+ "--------------------------------------\n\n\t\t\t\t\t"
+		    	+ "Person4\t4\t\t444$\n\n\t\tThis balance has been taken before any transaction occures"
+		    	+ "\n\n\t\t\tContact us for any queries at 01-999-392\n\n\n\t\t------------------------"
+		    	+ "---Have a nice day---------------------------\n",
+		    	"\t\t\t\tThis is an actual bank statement of Eskandar's Bank\t\t\t\t\n-----------------"
+		    	+ "-------------------------------------------------"
+		    	+ "---------------------------------------------------\n\t\t\t\t\tNames\t\tPins\t\tBalances"
+		    	+ "\n\n-------------------------------------------------------------------------------"
+		    	+ "--------------------------------------\n\n\t\t\t\t\t"
+		    	+ "Person5\t5\t\t555$\n\n\t\tThis balance has been taken before any transaction occures"
+		    	+ "\n\n\t\t\tContact us for any queries at 01-999-392\n\n\n\t\t-------------------"
+		    	+ "--------Have a nice day---------------------------\n"};
+
+*/
